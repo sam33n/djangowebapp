5ea01a6cdf790aa5d4ea3fa13330fd0948e1c056
sam33n <sheikhsameen@gmail.com>
2023-10-15 22:02:12 +0530

    added a class beta

--- djangowebapp-master/blog/forms.py
+++ djangowebapp-master/blog/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from . import models
 
-
+# added a class here
 class BlogCreate(forms.ModelForm):
     class Meta:
         model = models.Blog_mod
         fields = ['comment','name','email']
+
+
+class Beta:
+    pass
+
