e12e3e8996635c2378355ba800c794f1edb68f02
sam33n <sheikhsameen@gmail.com>
2023-10-15 22:06:04 +0530

    Revert "added a class beta"
    This reverts commit 5ea01a6cdf790aa5d4ea3fa13330fd0948e1c056.

--- djangowebapp-master/blog/forms.py
+++ djangowebapp-master/blog/forms.py
@@ -1,13 +1,8 @@
 from django import forms
 from . import models
 
-# added a class here
+
 class BlogCreate(forms.ModelForm):
     class Meta:
         model = models.Blog_mod
         fields = ['comment','name','email']
-
-
-class Beta:
-    pass
-
