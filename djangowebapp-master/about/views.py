df6deb19542c85e0d94830d574b5ac52d2d5b0b4
sheikhsameen <sheikhsameen@gmail.com>
2023-10-21 01:47:13 +0530

    added line

--- djangowebapp-master/about/views.py
+++ djangowebapp-master/about/views.py
@@ -4,3 +4,6 @@ from .models import About_mod
 def about(request):
     datas = About_mod.objects.all()
     return render(request, 'about/about.html',{ 'datas' : datas })
+
+
+# this is all about the database
